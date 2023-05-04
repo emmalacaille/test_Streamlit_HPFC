934e113c8a315645d3131b5db6ffc52b2a3c5e49
Emma LACAILLE <61498146+emmalacaille@users.noreply.github.com>
2023-05-04 08:47:20 +0200

    Update HPFC_app_bokeh.py

--- HPFC_app_bokeh.py
+++ HPFC_app_bokeh.py
@@ -2,7 +2,7 @@ import streamlit as st
 from datetime import datetime, timedelta
 import pandas as pd
 from bokeh.plotting import figure as bok
-from bokeh.models import DatetimeTickFormatter
+from bokeh.models import DatetimeTickFormatter, HoverTool
 
 from data_analysis import get_HPFC_data, merge_HPFC_data
 
@@ -34,13 +34,20 @@ with st.container():
     for c, country in enumerate(countries):
         with HPFC_Prognose[c]:
             st.header("HPFC Prognose - " + country)
-            prog = bok( x_axis_type='datetime', y_axis_label = "HPFC (EUR/MWh)" )
-            prog.plot_height=400
-            prog.plot_width=800
-            prog.xaxis.formatter = DatetimeTickFormatter(years="%Y", months="%d/%m/%Y", days="%d/%m/%Y", hours="%d/%m %Hh", hourmin="%d/%m %Hh%M",  minutes="%d/%m %Hh%Mmin%S")
+            prog = bok( width=800, height=400, x_axis_type='datetime', y_axis_label = "HPFC (EUR/MWh)" )
+            prog.xaxis.formatter = DatetimeTickFormatter(years="%Y", months="%d-%m-%Y", days="%d-%m-%Y", hours="%d-%m %Hh", hourmin="%d-%m %Hh%M",  minutes="%d-%m %Hh%Mmin%S")
             for d, date in enumerate(prog_dates):
-                prog.line(x=merged_df["Datum"], y=merged_df["HPFC_" + country + "_" + date], line_color=prog_colors[d], legend_label=date)
+                prog.line(source=merged_df, x="Datum", y="HPFC_" + country + "_" + date, line_color=prog_colors[d], legend_label=date)
             prog.legend.click_policy="hide"
+            prog_hover = HoverTool(
+                formatters={"@Datum": "datetime"},
+                tooltips=[
+                    ("Datum", "@Datum{%Y-%m-%d}"),
+                    ("HPFC_"+country+"_"+prog_dates[0], "@HPFC_"+country+"_"+prog_dates[0]),
+                    ("HPFC_"+country+"_"+prog_dates[1], "@HPFC_"+country+"_"+prog_dates[1]),
+                    ("HPFC_"+country+"_"+prog_dates[2], "@HPFC_"+country+"_"+prog_dates[2])
+            ])
+            prog.add_tools(prog_hover)
             st.bokeh_chart(prog, use_container_width=True)
  
 
@@ -65,9 +72,15 @@ with st.container():
 mittelwert_colors = [["blue", "dodgerblue"], ["forestgreen", "lawngreen"], ["red", "lightcoral"]]
 mittelwert_dates = [dates[d] for d in [1,2,3]]
 base_per_month = merged_df.set_index('Datum').groupby(pd.Grouper(freq='M')).mean()
+base_per_month.columns = [col_name+"_base" for col_name in base_per_month.columns]
 base_per_month["Datum"] = base_per_month.index.get_level_values('Datum')
+base_per_month = base_per_month.reset_index(drop=True)
 peak_per_month = peak_df.set_index('Datum').groupby(pd.Grouper(freq='M')).mean()
+peak_per_month.columns = [col_name+"_peak" for col_name in peak_per_month.columns]
 peak_per_month["Datum"] = peak_per_month.index.get_level_values('Datum')
+peak_per_month = peak_per_month.reset_index(drop=True)
+per_month =  pd.merge(base_per_month, peak_per_month, on="Datum")
+st.dataframe(per_month)
 with st.container():
     monatlicher_Mittelwerte = st.columns(len(countries))
     for c, country in enumerate(countries):
@@ -76,11 +89,18 @@ with st.container():
             mittelwert = bok( x_axis_type='datetime', y_axis_label = "HPFC (EUR/MWh)" )
             mittelwert.plot_height=400
             mittelwert.plot_width=800
-            mittelwert.xaxis.formatter = DatetimeTickFormatter(years="%Y", months="%d/%m/%Y", days="%d/%m/%Y", hours="%d/%m %Hh", hourmin="%d/%m %Hh%M",  minutes="%d/%m %Hh%Mmin%S")
+            mittelwert.xaxis.formatter = DatetimeTickFormatter(years="%Y", months="%d-%m-%Y", days="%d-%m-%Y", hours="%d-%m %Hh", hourmin="%d-%m %Hh%M",  minutes="%d-%m %Hh%Mmin%S")
+            mittelwert_tooltips=[("Datum", "@Datum{%Y-%m-%d}")]
             for d, date in enumerate(mittelwert_dates):
-                mittelwert.line(x=base_per_month["Datum"], y=base_per_month["HPFC_" + country + "_" + date], line_width=2, line_color=mittelwert_colors[d][0], legend_label=date+" - base")
-                mittelwert.line(x=peak_per_month["Datum"], y=peak_per_month["HPFC_" + country + "_" + date], line_width=2, line_color=mittelwert_colors[d][1], legend_label=date+" - peak")
+                mittelwert.line(source=per_month, x="Datum", y="HPFC_" + country + "_" + date + "_base", line_width=2, line_color=mittelwert_colors[d][0], legend_label=date+" - base")
+                mittelwert.line(source=per_month, x="Datum", y="HPFC_" + country + "_" + date + "_peak", line_width=2, line_color=mittelwert_colors[d][1], legend_label=date+" - peak")
+                mittelwert_tooltips.append( ("HPFC_"+country+"_"+date+" - base", "@HPFC_"+country+"_"+date+"_base") )
+                mittelwert_tooltips.append( ("HPFC_"+country+"_"+date+" - peak", "@HPFC_"+country+"_"+date+"_peak") )
             mittelwert.legend.click_policy="hide"
+            mittelwert_hover = HoverTool(
+                formatters={"@Datum": "datetime"},
+                tooltips=mittelwert_tooltips)
+            mittelwert.add_tools(mittelwert_hover)
             st.bokeh_chart(mittelwert, use_container_width=True)
         
-    
+  
